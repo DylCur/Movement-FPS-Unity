dbb40ee71645031ec74771465d405755a62b3024
DylCur <dylancurry001@gmail.com>
2024-07-12 19:50:06 +0100

    Began adding dashing

--- Assets/_Code/Player/PlayerController.cs
+++ Assets/_Code/Player/PlayerController.cs
@@ -1,54 +1,95 @@
 using System.Collections;
 using UnityEngine;
 
+// Random thought but i should add a detonator to this game for directional changes
+
+
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
-    public float sprintSpeed = 8f;
+
+    [Header("Walking")]
+
+    public float walkSpeed = 10f;
+    public float sprintSpeed = 16f;
     public float mouseSensitivity = 2f;
+    
+    [Space(10)]
+    [SerializeField] Vector3 moveVelocity;
+
+    [Header("Dashing")]
+
+    [SerializeField] float dashCD = 1f;
+    [SerializeField] float dashForce = 20f;
+    [SerializeField] bool canDash = true;
+    public KeyCode dashKey = KeyCode.LeftShift;
+    
+    [Space(10)]
+    [SerializeField] Vector3 dashVelocity;
+
+    bool shouldDash => canDash && Input.GetKeyDown(dashKey);
+    
+
+    // Other
 
     private Rigidbody rb;
     private Camera playerCamera;
 
     private float verticalLookRotation;
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        playerCamera = GetComponentInChildren<Camera>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
 
-    void Update()
-    {
-        HandleInput();
-        HandleMouseLook();
-    }
-
-    void HandleInput()
-    {
-
-        float moveSpeed = Input.GetKeyDown(KeyCode.LeftShift) ? sprintSpeed: walkSpeed;
-        // Debug.Log(moveSpeed);
-
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+    IEnumerator Dash(){
+        canDash = false;
+        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
 
         Vector3 moveDirection = transform.right * moveX + transform.forward * moveZ;
-        Vector3 moveVelocity = moveDirection.normalized;
+        dashVelocity = new Vector3(moveDirection.normalized.x * dashForce, rb.velocity.y, moveDirection.normalized.z * dashForce);
 
         // Apply movement to the Rigidbody
-        rb.velocity = new Vector3(moveVelocity.x * moveSpeed, rb.velocity.y, moveVelocity.z * moveSpeed);
+        rb.velocity = new Vector3(dashVelocity.x + moveVelocity.x, rb.velocity.y, dashVelocity.z + moveVelocity.z);
 
-        // Jumping (add your own conditions for jumping)
-        if (Input.GetButtonDown("Jump") && isGrounded())
-        {
-            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        // Pythagoras Theorem (I think this is right)
+        int i = 0;
+        bool debug = true;
+        while(Mathf.Sqrt((dashVelocity.x * dashVelocity.x) + (dashVelocity.z * dashVelocity.z)) > 0){
+
+            if(debug){
+                Debug.Log($"X^2 = {dashVelocity.x * dashVelocity.x} || Z^2 = {dashVelocity.z * dashVelocity.z} || Hypotenuse = {Mathf.Sqrt((dashVelocity.x * dashVelocity.x) + (dashVelocity.z * dashVelocity.z))} ");
+            }
+            
+            if(i == 10_000){
+                Debug.Log("Escaped after 10,000 iterations");
+                dashVelocity = Vector3.zero;
+                break;
+            }
+
+
+            if(dashVelocity.x - 0.1f >= 0){
+                dashVelocity.x -= 0.1f;
+            }
+
+            else{
+                dashVelocity.x = 0;
+            }
+            
+            if(dashVelocity.z - 0.1f >= 0){
+                dashVelocity.z -= 0.1f;
+            }
+
+            else{
+                dashVelocity.z = 0;
+            }
+
+            i++;
+           
         }
-    }
 
+        yield return new WaitForSeconds(dashCD);
+        
+        canDash = true;
+    }
+    
     bool isGrounded(){
         Vector3 position = new Vector3(transform.position.x, transform.position.y - transform.localScale.y, transform.position.z);
         Vector3 size = new Vector3(transform.localScale.x * 0.7f, 0.1f, transform.localScale.z * 0.7f);
@@ -65,6 +106,50 @@ public class PlayerController : MonoBehaviour
         }
         return false;
     }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        canDash = true;
+    }
+
+    void Update()
+    {
+        HandleInput();
+        HandleMouseLook();
+        if(shouldDash){
+            StartCoroutine(Dash());
+        }
+    }
+
+    void HandleInput()
+    {
+
+        float moveSpeed = Input.GetKeyDown(KeyCode.LeftShift) ? sprintSpeed: walkSpeed;
+        // Debug.Log(moveSpeed);
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * moveX + transform.forward * moveZ;
+        moveVelocity = moveDirection.normalized;
+
+        // Apply movement to the Rigidbody
+        rb.velocity = new Vector3(moveVelocity.x * moveSpeed + dashVelocity.x, rb.velocity.y, moveVelocity.z * moveSpeed);
+
+        // Jumping (add your own conditions for jumping)
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
+    }
+
+
+
+    
 
     void HandleMouseLook()
     {
